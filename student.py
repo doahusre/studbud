56cb251aa03c94498ae69e5f0c8c79c87b0b5c11
Payton Webber <paytonwebber@uvic.ca>
2024-05-25 18:43:21 +0000

    Create student dataobject

---
+++ student.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+
